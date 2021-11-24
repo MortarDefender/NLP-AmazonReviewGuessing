d9659416913f546ce06576657cc5b699539f418c
MortarDefender <63960808+MortarDefender@users.noreply.github.com>
2021-11-24 19:29:18 +0200

    comments added, improved functions

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-import hson
+import json
 import string
 import numpy as np
+from enum import Enum
 import matplotlib.pyplot as plt
 from collections import defaultdict
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer as Downscale
 
-from sklearn.linear_model import SGDClassifier
-from enum import Enum
 
 class ReviewClassifier():
     class __ReviewClass(Enum):
@@ -27,7 +27,6 @@ class ReviewClassifier():
         self.minWordsAmount = 1  # 3  # 1
         self.maxWordsAmount = 5  # 10 # 2
         self.maxFrequentCut = 11 # 14
-        self.arrayOfAppearances = list()        # can be temporary
         self.trainFileName = trainFileName
         self.distinctWords = dict()             # needed ??
         self.wordsFrequency = None              # needed ?? Well Yes But No
@@ -62,39 +61,18 @@ class ReviewClassifier():
     def __removeRedundancy(self, corpus):
         """ remove punctuation, stopwords and most common words from the corpus """
         
-        # punctuation = str.maketrans('', '', string.punctuation)
-        stopwords = ['i', 'I','me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", 
-                         "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 
-                         'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 
-                         'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 
-                         'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 
-                         'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 
-                         'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 
-                         'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 
-                         'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 
-                         'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've",
-                         'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', 
-                         "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', 
-                         "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', 
-                         "won't", 'wouldn', "wouldn't"]
-        
         for i, line in enumerate(corpus):
-            ## remove punctuation
-            corpus[i] = self.__removePunctuation(line) # ''.join(list(filter(lambda x: x != '', [line[row].translate(punctuation) for row in range(len(line))])))
-            
-            ## remove stopwords
-            corpus[i] = self.__removStopWords(corpus[i])  # " ".join(list(filter(lambda x: x not in stopwords and not x.isnumeric(), corpus[i].split())))
+            corpus[i] = self.__removePunctuation(line)
+            corpus[i] = self.__removStopWords(corpus[i])
             
         
         if self.wordsFrequency is None:
-            self.wordsFrequency = defaultdict(int)
-            ## create wordFrequency dictionary  -> can be done once
             allWords = " ".join(corpus)
             self.wordsFrequency = defaultdict(int)
+            
             for word in allWords.split():
                 self.wordsFrequency[word] += 1
         
-        ## get the 1000 most common words
         frequentWords = [keyValue[0] for keyValue in sorted(self.wordsFrequency.items(), key=lambda kv: kv[1], reverse=True)[:self.maxFrequentCut]]
         
         
@@ -108,6 +86,7 @@ class ReviewClassifier():
     def __removePunctuation(text):
         """ remove the punctuation from the text given and return it """
         punctuation = str.maketrans('', '', string.punctuation)
+        
         return ''.join(list(filter(lambda x: x != '', [text[row].translate(punctuation) for row in range(len(text))])))
     
     @staticmethod
@@ -127,13 +106,15 @@ class ReviewClassifier():
                          "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', 
                          "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', 
                          "won't", 'wouldn', "wouldn't"]
+        
         return  " ".join(list(filter(lambda x: x not in stopwords and not x.isnumeric(), text.split())))
     
     def __createBagOfWords(self):
         """ create the distinct words and words frequency dictionary """
         corpus = self.__removeRedundancy(self.__disassembleAllReviews())
-        self.arrayOfAppearances = self.vectorizer.fit_transform(corpus)
-
+        arrayOfAppearances = self.vectorizer.fit_transform(corpus)
+        
+        ## can be created and used to get the 1000 best feature
         # for i, word in enumerate(self.vectorizer.get_feature_names_out()):
         #     self.distinctWords[word] = i
 
@@ -142,7 +123,7 @@ class ReviewClassifier():
         #         self.wordsFrequency[word] += 1
 
         # self.wordsFrequency = sorted(self.wordsFrequency.items(), key=lambda kv: kv[1])[:1000]
-        return self.downsizer.fit_transform(self.arrayOfAppearances)
+        return self.downsizer.fit_transform(arrayOfAppearances)
     
     @staticmethod
     def __printPrediction(testReviewsData, predicted):
@@ -232,6 +213,7 @@ class ReviewClassifier():
         return self.__getPredictedData(predicted, trueResults)
     
     def __testPossibilities(self, testFileName, minRange, maxRange, maxCut):
+        """ get the maximum possible accuracy of the model using an iteration over all the posiibilites """
         max_test_results = None
         current_test_results = {}
         n, s, e = 0, 0, 0
@@ -258,15 +240,18 @@ class ReviewClassifier():
         return max_test_results
     
     def fitNaiveBayes(self, testFileName):
+        """ get the result of the model over the Naive Classifier """
         self.classifier = MultinomialNB()
-        return self.__testPossibilities(testFileName, 3, 7, 15)
-        # return self.__getFitResults(testFileName)
+        # return self.__testPossibilities(testFileName, 3, 7, 15)
+        return self.__getFitResults(testFileName)
     
     def fitLogisticRegression(self, testFileName):
+        """ get the result of the model over the Logistic Regression Classifier """
         self.classifier = LogisticRegression(random_state = 0)
         return self.__getFitResults(testFileName)
     
     def fitSVM(self, testFileName):
+        """ get the result of the model over the SVM Classifier """
         self.classifier = SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, random_state=42, max_iter=5, tol=None)
         return self.__getFitResults(testFileName)
     
